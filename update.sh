1ffc54e8bfec09e4fe7c40dad259a6bc29268072
Chetan Goti <chetan@dreamworldsolutions.com>
2022-02-02 18:48:52 +0530

    Mautic 4.1.2

--- update.sh
+++ update.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
 
-current="$(curl https://api.github.com/repos/mautic/mautic/releases/latest -s | jq -r .name)"
+## we have disabled both command. because we faced a issue when we are upgrading mautic version to 3.2.1 and in mautic relases notes it showing latest version for mautic is available is 2.16.4. so. that's why we decided to use static version to ignore this ERROR.
+# REF: https://github.com/mautic/mautic/releases/latest
+#current="$(curl https://api.github.com/repos/mautic/mautic/releases/latest -s | jq -r .name)"
+#current="$(echo $current | sed 's/Mautic Community //g')"
+
+# mautic version
+current=4.1.2
+
+echo "Mautic version is: $current"
 
 # TODO - Expose SHA signatures for the packages somewhere
 wget -O mautic.zip https://github.com/mautic/mautic/releases/download/$current/$current.zip
